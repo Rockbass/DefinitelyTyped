93732bd6418a21d07f1d1cbe14e648541b63dbff
Vladimir Ponomar <v.v.ponomar@gmail.com>
2019-06-24 10:12:26 +0300

    bitcoin-address-validation init

---
+++ types/bitcoin-address-validation/bitcoin-address-validation-tests.ts
@@ -0,0 +1,3 @@
+import validateBtcAddress from './';
+
+console.log(validateBtcAddress('17VZNX1SN5NtKa8UQFxwQbFeFc3iqRYhem'));
---
+++ types/bitcoin-address-validation/index.d.ts
@@ -0,0 +1,15 @@
+// Type definitions for bitcoin-address-validation 0.2
+// Project: https://github.com/ruigomeseu/bitcoin-address-validation
+// Definitions by: Vladimir Ponomar <https://github.com/rockbass>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/// <reference types="node"/>
+
+export interface ValidationResponse {
+    address: string;
+    type: string;
+    testnet: false;
+    bech32: false;
+}
+
+export function validateBtcAddress(address: any): ValidationResponse | boolean;
